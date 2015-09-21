413d7aa311ace3e558b6ef194fd8e94d7fdd6045
ckakin <ckakin@gmail.com>
2015-09-21 11:25:38 +0800

    setup basic spring boot structure and config

---
+++ src/main/java/com/compare/Application.java
@@ -0,0 +1,9 @@
+package com.compare;
+
+import org.springframework.boot.SpringApplication;
+
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
